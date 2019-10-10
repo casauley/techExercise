97da9369a4aab0c096c23ddda71845cc0a34d7a3
casauley <casauley@yahoo.com>
2019-10-09 23:39:02 -0500

    Final Upload.

---
+++ webproject-techExercise-Sauley/src/InsertSauley.java
@@ -0,0 +1,84 @@
+
+/**
+ * @file SimpleFormInsert.java
+ */
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/SimpleFormInsert")
+public class InsertSauley extends HttpServlet {
+   private static final long serialVersionUID = 1L;
+
+   public InsertSauley() {
+      super();
+   }
+
+   protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+      String item = request.getParameter("item");
+      PrintWriter out = response.getWriter();
+      String title = "Database Result";
+      String docType = "<!doctype html public \"-//w3c//dtd html 4.0 " + //
+            "transitional//en\">\n"; //
+      out.println(docType + //
+            "<html>\n" + //
+            "<head><title>" + title + "</title></head>\n" + //
+            "<body bgcolor=\"#f0f0f0\">\n" + //
+            "<h1 align=\"center\">" + title + "</h1>\n");
+
+      Connection connection = null;
+      String insertSql = " INSERT INTO MyTableSauleyToDoList (id, ITEM) values (default, ?)";
+
+      try {
+    	  // Grab item to insert
+         DBConnectionSauley.getDBConnection();
+         connection = DBConnectionSauley.connection;
+         PreparedStatement preparedStmtInsert = connection.prepareStatement(insertSql);
+         preparedStmtInsert.setString(1, item);
+         preparedStmtInsert.execute();
+         
+         // Display items after insert
+         String displaySQL = "SELECT * FROM MyTableSauleyToDoList";
+         PreparedStatement preparedStmtDisplay = connection.prepareStatement(displaySQL);
+         //preparedStmtDisplay.executeQuery();
+         
+         ResultSet rs = preparedStmtDisplay.executeQuery();
+         while (rs.next()) {
+             int id = rs.getInt("id");
+             String itemToDisplay = rs.getString("item").trim();
+             out.print("Item " + id + ": ");
+             out.println(itemToDisplay + "<br>");
+             
+          }
+         
+         connection.close();
+      } catch (Exception e) {
+         e.printStackTrace();
+      }
+
+      // Set response content type
+      response.setContentType("text/html");
+      out.println(docType + //
+            "<html>\n" + //
+            "<ul>\n" + //
+
+            "</ul>\n");
+
+      out.println("<br> <br> <a href=/webproject-techExercise-Sauley/insert_sauley.html>Insert Item</a> <br>");
+      out.println("      <a href=/webproject-techExercise-Sauley/remove_sauley.html>Remove Item</a> <br>");
+      out.println("</body></html>");
+   }
+   
+   protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+      doGet(request, response);
+   }
+
+}
---
+++ webproject-techExercise-Sauley/src/RemoveSauley.java
@@ -0,0 +1,105 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/SimpleFormSearch")
+public class RemoveSauley extends HttpServlet {
+   private static final long serialVersionUID = 1L;
+
+   public RemoveSauley() {
+      super();
+   }
+
+   protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+      String keyword = request.getParameter("keyword");
+      search(keyword, response);
+   }
+
+   void search(String keyword, HttpServletResponse response) throws IOException {
+      response.setContentType("text/html");
+      PrintWriter out = response.getWriter();
+      String title = "Database Result";
+      String docType = "<!doctype html public \"-//w3c//dtd html 4.0 " + //
+            "transitional//en\">\n"; //
+      out.println(docType + //
+            "<html>\n" + //
+            "<head><title>" + title + "</title></head>\n" + //
+            "<body bgcolor=\"#f0f0f0\">\n" + //
+            "<h1 align=\"center\">" + title + "</h1>\n");
+
+      Connection connection = null;
+      PreparedStatement preparedStatement = null;
+      try {
+         DBConnectionSauley.getDBConnection();
+         connection = DBConnectionSauley.connection;
+
+         if (keyword.isEmpty()) {
+        	 out.println("Nothing was removed. Please enter an id to remove an item next time.");
+         } else {
+            String selectSQL = "DELETE FROM MyTableSauleyToDoList WHERE id = ?";
+            String itemID = keyword;
+            preparedStatement = connection.prepareStatement(selectSQL);
+            preparedStatement.setString(1, itemID);
+            preparedStatement.execute();
+            
+            // Display items after Removal
+            String displaySQL = "SELECT * FROM MyTableSauleyToDoList";
+            PreparedStatement preparedStmtDisplay = connection.prepareStatement(displaySQL);
+            //preparedStmtDisplay.executeQuery();
+            
+            ResultSet rs = preparedStmtDisplay.executeQuery();
+            while (rs.next()) {
+                int id = rs.getInt("id");
+                String itemToDisplay = rs.getString("item").trim();
+                out.print("Item " + id + ": ");
+                out.println(itemToDisplay + "<br>");
+                
+             }
+         }
+         
+         // Set response content type
+         response.setContentType("text/html");
+         out.println(docType + //
+               "<html>\n" + //
+               "<ul>\n" + //
+
+               "</ul>\n");
+         
+         out.println("<br><br><a href=/webproject-techExercise-Sauley/insert_sauley.html>Insert Item</a> <br>");
+         out.println("      <a href=/webproject-techExercise-Sauley/remove_sauley.html>Remove Item</a> <br>");
+         out.println("</body></html>");
+         preparedStatement.close();
+         connection.close();
+      } catch (SQLException se) {
+         se.printStackTrace();
+      } catch (Exception e) {
+         e.printStackTrace();
+      } finally {
+         try {
+            if (preparedStatement != null)
+               preparedStatement.close();
+         } catch (SQLException se2) {
+         }
+         try {
+            if (connection != null)
+               connection.close();
+         } catch (SQLException se) {
+            se.printStackTrace();
+         }
+      }
+   }
+
+   protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+      doGet(request, response);
+   }
+
+}
